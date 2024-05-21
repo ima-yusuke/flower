81a3af7b50b361b7eb213cd94576c36985b5cf05
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-05-21 10:22:34 +0900

    fixed:管理画面/sortable.js修正

--- resources/js/sortable.js
+++ resources/js/sortable.js
@@ -41,16 +41,14 @@ let questions = document.getElementsByClassName("qa__item");
 let editBtns = document.getElementsByClassName("editBtn");
 
 for (let i=0;i<questions.length;i++){
-    editBtns[i].addEventListener("click",function (){
-        const elem = document.getElementsByClassName("answer_sortable");
-        Sortable.create(elem[i], {
-            animation: 150,
-            onStart:  onStartEvent,  // 2-1, ドラッグ開始時
-            onEnd:    onEndEvent,    // 2-2, ドラッグ終了時
-            onChange: onChangeEvent, // 2-3, ドラッグ変化時
-            onSort:   onSortEventAnswer   // 2-4, 並び替え終了時
-        });
-    })
+    const elem = document.getElementsByClassName("answer_sortable");
+    Sortable.create(elem[i], {
+        animation: 150,
+        onStart:  onStartEvent,  // 2-1, ドラッグ開始時
+        onEnd:    onEndEvent,    // 2-2, ドラッグ終了時
+        onChange: onChangeEvent, // 2-3, ドラッグ変化時
+        onSort:   onSortEventAnswer   // 2-4, 並び替え終了時
+    });
 }
 
 function onSortEventAnswer(e){
