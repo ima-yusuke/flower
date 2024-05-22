7bcd64be23c770f585ee3cf3a2464f460f5deb20
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-05-22 15:06:22 +0900

    update:ユーザー/view（案）

--- resources/views/flower.blade.php
+++ resources/views/flower.blade.php
@@ -58,7 +58,7 @@
                         </button>
 
                         {{--modal open button--}}
-                        <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="btn block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button">
+                        <button data-modal-target="default-modal" data-modal-toggle="default-modal" id="open-modal-btn" class="btn block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button">
                             購入する
                         </button>
                     </aside>
@@ -84,8 +84,8 @@
                                 </div>
                                 <!-- Modal footer -->
                                 <div class="flex items-center justify-between p-4 md:p-5 border-t border-gray-200 rounded-b">
-                                    <button data-modal-hide="default-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">閉じる</button>
-                                    <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">送信</button>
+                                    <button data-modal-hide="default-modal" type="button" class="mail-btn py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">閉じる</button>
+                                    <button data-modal-hide="default-modal" type="button" class="mail-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">送信</button>
                                 </div>
                             </div>
                         </div>
@@ -139,6 +139,15 @@
     })
 
     products = tmpArray;
+
+    // 購入ボタンを押したときにmodalでbodyのoverflow-hiddenが外れる問題に対応
+    let mail_btn = document.getElementsByClassName("mail-btn");
+    for (let i = 0; i < mail_btn.length; i++) {
+        mail_btn[i].addEventListener("click", function () {
+            let body = document.querySelector("body");
+            body.style.overflow = "hidden";
+        });
+    }
 
 </script>
 </x-layout>
